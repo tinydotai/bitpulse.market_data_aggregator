d7abea068cf200a37df5ee893b1dc210fc38ad11
luka <edulukalortk@gmail.com>
2024-10-05 10:28:02 +0000

    pairs from sys args

--- src/get_binance_transactions.py
+++ src/get_binance_transactions.py
@@ -1,13 +1,16 @@
 import asyncio
 import sys
 from binance.transactions import main
-from binance.pairs import usdt_pairs
 
 # Parse command-line arguments
-args = dict(arg.split('=') for arg in sys.argv[1:])
+args = dict(arg.split('=', 1) for arg in sys.argv[1:])
 DB_NAME = args.get('db_name', 'bitpulse')
 STATS_COLLECTION = args.get('stats_collection', 'binance_transactions')
 BIG_TRANSACTIONS_COLLECTION = args.get('big_transactions_collection', 'binance_big_transactions')
 
+# Parse usdt_pairs from command-line argument
+usdt_pairs_str = args.get('pairs', '')
+PAIRS = [pair.strip() for pair in usdt_pairs_str.split(',')] if usdt_pairs_str else []
+
 if __name__ == "__main__":
-    asyncio.run(main(DB_NAME, STATS_COLLECTION, BIG_TRANSACTIONS_COLLECTION, usdt_pairs[:2]))
+    asyncio.run(main(DB_NAME, STATS_COLLECTION, BIG_TRANSACTIONS_COLLECTION, PAIRS))
